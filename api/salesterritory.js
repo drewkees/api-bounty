cca21b6664f59e9ff7c46b36156491855009a1ab
Andrew C. Baldonado <acbaldonado@bounty.local>
2025-12-16 09:21:04 +0800

    api

--- api/salesterritory.js
+++ api/salesterritory.js
@@ -1,6 +1,16 @@
 const { getColumn } = require("../lib/sheets");
 
 module.exports = async (req, res) => {
+  // Add CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow all origins
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   try {
     const data = await getColumn("SALES TERRITORY!A:A");
     res.status(200).json(data);
